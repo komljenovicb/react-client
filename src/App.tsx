d549d8f7383e56a831f6b9877cc7466352fd5a64
Bojana Komljenović <komljenovic.bojana.1@gmail.com>
2019-07-13 00:32:53 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import IzmenaUsluge from './pages/IzmenaUsluge';
 import UnosUsluge from './pages/UnosUsluge';
-import PretragaIzvestaja from './pages/PretragaIzvestaja';
-import IzmenaIzvestaja from './pages/IzmenaIzvestaja';
-import UnosIzvestaja from './pages/UnosIzvestaja';
-import PrikazIzvestaja from './pages/PrikazIzvestaja';
 import logo from "./assets/images/logo.png";
 
 class App extends Component {
@@ -25,28 +21,12 @@ class App extends Component {
 					</li>
 					<li>
 						<Link to="/izmenausluge" className="nav-link">Izmena usluge</Link>
-					</li>
-					<li>
-						<Link to="/pretragaizvestaja" className="nav-link">Pretraga izveštaja</Link>
-					</li>
-					<li>
-						<Link to="/unosizvestaja" className="nav-link">Unos izveštaja</Link>
-					</li>
-					<li>
-						<Link to="/izmenaizvestaja" className="nav-link">Izmena izveštaja</Link>
-					</li>
-					<li>
-						<Link to="/prikazizvestaja" className="nav-link">Prikaz izveštaja</Link>
-					</li>						
+					</li>					
 				  </ul>
 				</div>
 			  </nav>
 			  <Route path="/unosusluge" component={UnosUsluge} />
 			  <Route path="/izmenausluge" component={IzmenaUsluge} />
-			  <Route path="/pretragaizvestaja" component={PretragaIzvestaja} />
-			  <Route path="/unosizvestaja" component={UnosIzvestaja} />
-			  <Route path="/izmenaizvestaja" component={IzmenaIzvestaja} />
-			  <Route path="/prikazizvestaja" component={PrikazIzvestaja} />
 			</div>
 			<div className="footer">
 					<h6>&copy;FON, 2019</h6>
