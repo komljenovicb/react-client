feeea897b021a820b1d1d558e31e15fa6571f046
Bojana Komljenović <komljenovic.bojana.1@gmail.com>
2019-07-15 11:25:42 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ class App extends Component {
 			  <Route path="/izmenausluge" component={IzmenaUsluge} />
 			</div>
 			<div className="footer">
-					<h6>&copy;FON, 2019</h6>
+				<h6>&copy;FON, 2019</h6>
 			</div>
 		  </div>
       </Router>
